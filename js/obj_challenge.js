a334a303c4be0031ee52606403e7b13f5f0596df
Alex Peck <alex@alxpck.com>
2018-01-08 22:05:54 -0500

    object challenge part 1

---
+++ js/obj_challenge.js
@@ -0,0 +1,7 @@
+var students = [
+  {name: 'Alex', track: 'JavaScript', achievements: 42, points: 880},
+  {name: 'Sarah', track: 'PHP', achievements: 12, points: 5},
+  {name: 'Leo', track: 'Python', achievements: 56, points: 44},
+  {name: 'Robert', track: 'Ruby', achievements: 78, points: 8860},
+  {name: 'Monty', track: 'Python', achievements: 5000, points: 180},
+];
